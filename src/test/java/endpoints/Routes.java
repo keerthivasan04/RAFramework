cb517a7533784867d6bc5a95396945c3ab9588b2
keerthivasan04 <nkeerthivasan04@gmail.com>
2024-12-17 09:55:45 +0530

    Day1

---
+++ src/test/java/endpoints/Routes.java
@@ -0,0 +1,19 @@
+package endpoints;
+
+public class Routes {
+    /*
+    URL -- https://petstore.swagger.io/#
+    post -- https://petstore.swagger.io/#/user/createUsersWithListInput
+    get -- https://petstore.swagger.io/#/user/getUserByName
+    put -- https://petstore.swagger.io/#/user/updateUser
+    delete -- https://petstore.swagger.io/#/user/deleteUser
+     */
+
+    public static String base_url = "https://petstore.swagger.io/#";
+
+    //user module
+    public static String post_url = base_url + "/user/{username}";
+    public static String get_url = base_url + "/user/{username}";
+    public static String update_url = base_url + "/user/{username}";
+    public static String delete_url = base_url + "/user/{username}";
+}
---
+++ src/test/java/endpoints/UserEndPoints.java
@@ -0,0 +1,9 @@
+package endpoints;
+
+
+// UserEndPoints.java class
+// Created to perform Create, Read, Update, Delete requests of the user API
+
+public class UserEndPoints {
+
+}
