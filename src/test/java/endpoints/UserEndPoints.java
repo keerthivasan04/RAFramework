8147a5c57f46597328e756617d3dd3d5553a8c07
keerthivasan04 <nkeerthivasan04@gmail.com>
2024-12-17 10:14:44 +0530

    update

--- src/test/java/endpoints/UserEndPoints.java
+++ src/test/java/endpoints/UserEndPoints.java
@@ -4,13 +4,20 @@ package endpoints;
 // UserEndPoints.java class
 // Created to perform Create, Read, Update, Delete requests of the user API
 
+import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
+
+import static endpoints.Routes.get_url;
+import static io.restassured.RestAssured.given;
 
 public class UserEndPoints {
 
     @Test
     void getTest() {
-
+        given().contentType(ContentType.JSON)
+                .when()
+                .get(get_url)
+                .then().log().all();
     }
 
 }
