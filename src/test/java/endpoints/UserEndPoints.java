5b372ae2be5842410a09e7ce09667d855b3dc14f
keerthivasan04 <nkeerthivasan04@gmail.com>
2024-12-17 10:01:21 +0530

    rectify issue

--- src/test/java/endpoints/UserEndPoints.java
+++ src/test/java/endpoints/UserEndPoints.java
@@ -4,6 +4,13 @@ package endpoints;
 // UserEndPoints.java class
 // Created to perform Create, Read, Update, Delete requests of the user API
 
+import org.testng.annotations.Test;
+
 public class UserEndPoints {
+
+    @Test
+    void getTest() {
+
+    }
 
 }
